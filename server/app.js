0423b74b21e1c1775467509c313785052b9fa8ed
Katie Roy <kmr8172@g.rit.edu>
2020-04-11 22:55:20 -0400

    changed ReptileMaker to ReptileRecollection

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/ReptileMaker';
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/ReptileRecollection';
 
 // Setup mongoose options to use newer functionality
 const mongooseOptions = {
